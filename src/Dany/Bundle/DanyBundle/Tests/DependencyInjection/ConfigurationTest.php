ad2ddbdf93010b62ced66fa73a5f23ec0436b9b0
Mario Škrlec <whitepostmail@gmail.com>
2017-08-26 12:28:38 +0200

    Initial dany configuration fully tested

--- src/Dany/Bundle/DanyBundle/Tests/DependencyInjection/ConfigurationTest.php
+++ src/Dany/Bundle/DanyBundle/Tests/DependencyInjection/ConfigurationTest.php
@@ -16,17 +16,42 @@ class ConfigurationTest extends KernelTestCase
             file_get_contents(__DIR__.'/config/minimal_valid_config.yml')
         );
 
-        $configs = array($minimalDany);
+        $processor = new Processor();
+        $configuration = new Configuration();
+
+        $processedConfiguration = $processor->processConfiguration(
+            $configuration,
+            $minimalDany
+        );
+
+        $categoryApp = $processedConfiguration['resources']['app.category'];
+
+        $this->assertEquals('AppBundle\Entity\Category', $categoryApp['model']);
+        $this->assertEmpty($categoryApp['models']);
+        $this->assertEmpty($categoryApp['response_headers']);
+        $this->assertEmpty($categoryApp['flow']);
+    }
+
+    public function test_full_configuration()
+    {
+        $minimalDany = Yaml::parse(
+            file_get_contents(__DIR__.'/config/full_valid_config.yml')
+        );
 
         $processor = new Processor();
         $configuration = new Configuration();
 
         $processedConfiguration = $processor->processConfiguration(
             $configuration,
-            $configs
+            $minimalDany
         );
 
-        dump($processedConfiguration);
-        die();
+        $categoryApp = $processedConfiguration['resources']['app.category'];
+
+        $this->assertNotEmpty($categoryApp['models']);
+        $this->assertNotEmpty($categoryApp['response_headers']);
+        $this->assertNotEmpty($categoryApp['listeners']);
+        $this->assertNotEmpty($categoryApp['flow']);
+        $this->assertNull($categoryApp['model']);
     }
 }
